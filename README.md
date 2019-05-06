6755838a7d1694afdb2e5cbbc3e2906c57cae4a9
Graham Dyson <grahamdyson@hotmail.com>
2019-05-06 08:53:30 +0100

    added count and restrict function uses identifiers defined by the client to support multiple requests for the same stream only handles a single maximum concurrent stream that never expires

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Concurrent (Video Stream) Restriction Service
 =
 
 This service counts and restricts the number of concurrent video streams being watched by a user.
+
+Clients must provide unique identifiers for the video streams. This means the service can support multiple requests for the same stream (i.e. doesn't count the same stream more than once).
---
+++ countAndRestrict/test.js
@@ -0,0 +1,41 @@
+const countAndRestrict = require(".");
+
+test(
+	"Current identifier of null returns identifier to watch as current and maximum not exceeded",
+	() => {
+		const identifierToWatch = {};
+
+		expect(
+			countAndRestrict({
+				currentIdentifier: null,
+				identifierToWatch,
+			}),
+		)
+		.toEqual(
+			{
+				currentIdentifier: [ identifierToWatch ],
+				isMaximumExceeded: false,
+			},
+		);
+	},
+);
+
+test(
+	"Current identifier of value returns current and maximum exceeded",
+	() => {
+		const currentIdentifier = {};
+
+		expect(
+			countAndRestrict({
+				currentIdentifier,
+				identifierToWatch: {},
+			}),
+		)
+		.toEqual(
+			{
+				currentIdentifier,
+				isMaximumExceeded: true,
+			},
+		);
+	},
+);
